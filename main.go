b897fd20882c0d70fd2d218cfa30cf2f917e6161
Bingkun Yang <bingkun.yang@K2QQR0P936.vungle.com>
2023-06-25 15:47:36 +0800

    add main.go

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+        "context"
+        "fmt"
+        "io/ioutil"
+        "log"
+        "os"
+
+        "golang.org/x/oauth2/clientcredentials"
+)
+
+func main() {
+        var oauthConfig = &clientcredentials.Config{
+                ClientID:     os.Getenv("OAUTH_CLIENT_ID"),
+                ClientSecret: os.Getenv("OAUTH_CLIENT_SECRET"),
+                TokenURL:     "https://api.tailscale.com/api/v2/oauth/token",
+        }
+
+        client := oauthConfig.Client(context.Background())
+        // Replace example.com with your tailnet name.
+        resp, err := client.Get("https://api.tailscale.com/api/v2/tailnet/example.com/devices")
+        if err != nil {
+                log.Fatalf("error getting keys: %v", err)
+        }
+
+        body, err := ioutil.ReadAll(resp.Body)
+        if err != nil {
+                log.Fatalf("error reading response body: %v", err)
+        }
+
+        fmt.Printf("response: %s", string(body))
+}
