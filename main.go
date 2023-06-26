fd1cc5eaa189371b30bcb2d2dad6a11016dbffe2
Bingkun Yang <bingkun.yang@K2QQR0P936.vungle.com>
2023-06-26 12:32:01 +0800

    add secret get

--- main.go
+++ main.go
@@ -83,45 +83,79 @@ func main() {
 	// fmt.Printf("create_keys_body response: %s", string(createKeysBody))		
 	
 	// Section to call Vault api
-	getSecretWithAWSAuthIAM()
-	
-}
-
-func getSecretWithAWSAuthIAM() (string, error) {
+	// getSecretWithAWSAuthIAM()
 	config := vault.DefaultConfig() // modify for more granular configuration
 
-	client, err := vault.NewClient(config)
-	if err != nil {
-		return "", fmt.Errorf("unable to initialize Vault client: %w", err)
+	vaultClient, vaultErr := vault.NewClient(config)
+	if vaultErr != nil {
+		log.Fatalf("unable to initialize Vault client: %s", vaultErr)
 	}
 
 	awsAuth, err := auth.NewAWSAuth(
 		auth.WithRole("vault-prod"), // if not provided, Vault will fall back on looking for a role with the IAM role name if you're using the iam auth type, or the EC2 instance's AMI id if using the ec2 auth type
 	)
 	if err != nil {
-		return "", fmt.Errorf("unable to initialize AWS auth method: %w", err)
+		log.Fatalf("unable to initialize AWS auth method: %s", err)
 	}
 
-	authInfo, err := client.Auth().Login(context.Background(), awsAuth)
+	authInfo, err := vaultClient.Auth().Login(context.Background(), awsAuth)
 	if err != nil {
-		return "", fmt.Errorf("unable to login to AWS auth method: %w", err)
+		log.Fatalf("unable to login to AWS auth method: %s", err)
 	}
 	if authInfo == nil {
-		return "", fmt.Errorf("no auth info was returned after login")
+		log.Fatalf("no auth info was returned after login:")
 	}
 
 	// get secret from the default mount path for KV v2 in dev mode, "secret"
-	secret, err := client.KVv2("secret").Get(context.Background(), "creds")
+	secret, err := vaultClient.KVv2("secret").Get(context.Background(), "creds")
 	if err != nil {
-		return "", fmt.Errorf("unable to read secret: %w", err)
+		log.Fatalf("unable to read secret: %s", err)
 	}
 
 	// data map can contain more than one key-value pair,
 	// in this case we're just grabbing one of them
 	value, ok := secret.Data["test_password"].(string)
 	if !ok {
-		return "", fmt.Errorf("value type assertion failed: %T %#v", secret.Data["test_password"], secret.Data["test_password"])
-	}
-
-	return value, nil
+		log.Fatalf("value type assertion failed: %T %#v", secret.Data["test_password"], secret.Data["test_password"])
+	}	
+	fmt.Printf("value response: %s", value)	
 }
+
+// func getSecretWithAWSAuthIAM() (string, error) {
+// 	config := vault.DefaultConfig() // modify for more granular configuration
+
+// 	client, err := vault.NewClient(config)
+// 	if err != nil {
+// 		return "", fmt.Errorf("unable to initialize Vault client: %w", err)
+// 	}
+
+// 	awsAuth, err := auth.NewAWSAuth(
+// 		auth.WithRole("vault-prod"), // if not provided, Vault will fall back on looking for a role with the IAM role name if you're using the iam auth type, or the EC2 instance's AMI id if using the ec2 auth type
+// 	)
+// 	if err != nil {
+// 		return "", fmt.Errorf("unable to initialize AWS auth method: %w", err)
+// 	}
+
+// 	authInfo, err := client.Auth().Login(context.Background(), awsAuth)
+// 	if err != nil {
+// 		return "", fmt.Errorf("unable to login to AWS auth method: %w", err)
+// 	}
+// 	if authInfo == nil {
+// 		return "", fmt.Errorf("no auth info was returned after login")
+// 	}
+
+// 	// get secret from the default mount path for KV v2 in dev mode, "secret"
+// 	secret, err := client.KVv2("secret").Get(context.Background(), "creds")
+// 	if err != nil {
+// 		return "", fmt.Errorf("unable to read secret: %w", err)
+// 	}
+
+// 	// data map can contain more than one key-value pair,
+// 	// in this case we're just grabbing one of them
+// 	value, ok := secret.Data["test_password"].(string)
+// 	if !ok {
+// 		return "", fmt.Errorf("value type assertion failed: %T %#v", secret.Data["test_password"], secret.Data["test_password"])
+// 	}
+
+// 	return value, nil
+// }
