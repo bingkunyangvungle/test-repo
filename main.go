1a8fec9772fa6a63a5f1a37440cbc7928535ed27
Bingkun Yang <bingkun.yang@K2QQR0P936.vungle.com>
2023-06-26 18:15:14 +0800

    change back to using role

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ import (
 	// "encoding/json"
 	// "bytes"
 	vault "github.com/hashicorp/vault/api"
-	// auth "github.com/hashicorp/vault/api/auth/aws"
+	auth "github.com/hashicorp/vault/api/auth/aws"
 )
 
 func main() {
@@ -86,78 +86,78 @@ func main() {
 	config := vault.DefaultConfig() // modify for more granular configuration
 	config.Address = "https://vault.ops.vungle.io"
 
-	// vaultClient, vaultErr := vault.NewClient(config)
-	// if vaultErr != nil {
-	// 	log.Fatalf("unable to initialize Vault client: %s", vaultErr)
-	// }
+	vaultClient, vaultErr := vault.NewClient(config)
+	if vaultErr != nil {
+		log.Fatalf("unable to initialize Vault client: %s", vaultErr)
+	}
 
-	// awsAuth, err := auth.NewAWSAuth(
-	// 	auth.WithRole("vault-prod"), // if not provided, Vault will fall back on looking for a role with the IAM role name if you're using the iam auth type, or the EC2 instance's AMI id if using the ec2 auth type
-	// )
-	// if err != nil {
-	// 	log.Fatalf("unable to initialize AWS auth method: %s", err)
-	// }
+	awsAuth, err := auth.NewAWSAuth(
+		auth.WithRole("vault-prod"), // if not provided, Vault will fall back on looking for a role with the IAM role name if you're using the iam auth type, or the EC2 instance's AMI id if using the ec2 auth type
+	)
+	if err != nil {
+		log.Fatalf("unable to initialize AWS auth method: %s", err)
+	}
 	
-	// authInfo, err := vaultClient.Auth().Login(context.Background(), awsAuth)
-	// if err != nil {
-	// 	log.Fatalf("unable to login to AWS auth method: %s", err)
-	// }
-	// if authInfo == nil {
-	// 	log.Fatalf("no auth info was returned after login:")
-	// }
+	authInfo, err := vaultClient.Auth().Login(context.Background(), awsAuth)
+	if err != nil {
+		log.Fatalf("unable to login to AWS auth method: %s", err)
+	}
+	if authInfo == nil {
+		log.Fatalf("no auth info was returned after login:")
+	}
 
-	// // get secret from the default mount path for KV v2 in dev mode, "secret"
-	// secret, err := vaultClient.KVv2("secret").Get(context.Background(), "creds")
-	// if err != nil {
-	// 	log.Fatalf("unable to read secret: %s", err)
-	// }
+	// get secret from the default mount path for KV v2 in dev mode, "secret"
+	secret, err := vaultClient.KVv2("secret").Get(context.Background(), "creds")
+	if err != nil {
+		log.Fatalf("unable to read secret: %s", err)
+	}
 
-	// // data map can contain more than one key-value pair,
-	// // in this case we're just grabbing one of them
-	// value, ok := secret.Data["test_password"].(string)
-	// if !ok {
-	// 	log.Fatalf("value type assertion failed: %T %#v", secret.Data["test_password"], secret.Data["test_password"])
-	// }	
-	// fmt.Printf("value response: %s", value)	
+	// data map can contain more than one key-value pair,
+	// in this case we're just grabbing one of them
+	value, ok := secret.Data["test_password"].(string)
+	if !ok {
+		log.Fatalf("value type assertion failed: %T %#v", secret.Data["test_password"], secret.Data["test_password"])
+	}	
+	fmt.Printf("value response: %s", value)	
 	
 	
 	// Use token to do
-	vaultClient, err := vault.NewClient(config)
-	if err != nil {
-		log.Fatalf("unable to initialize Vault client: %v", err)
-	}	
+	// vaultClient, err := vault.NewClient(config)
+	// if err != nil {
+	// 	log.Fatalf("unable to initialize Vault client: %v", err)
+	// }	
 	
-	// Authenticate
-	vaultClient.SetToken(os.Getenv("PERSONAL_VAULT_TOKEN"))
+	// // Authenticate
+	// vaultClient.SetToken(os.Getenv("PERSONAL_VAULT_TOKEN"))
 
-	secretData := map[string]interface{}{
-		"bingkun_password": "Hashi123",
-	}
+	// secretData := map[string]interface{}{
+	// 	"bingkun_password": "Hashi123",
+	// }
 
-	// Write a secret
-	_, err = vaultClient.KVv2("secret").Put(context.Background(), "bingkun-secret-password", secretData)
-	if err != nil {
-		log.Fatalf("unable to write secret: %v", err)
-	}
+	// // Write a secret
+	// _, err = vaultClient.KVv2("secret").Put(context.Background(), "bingkun-secret-password", secretData)
+	// if err != nil {
+	// 	log.Fatalf("unable to write secret: %v", err)
+	// }
 
-	fmt.Println("Secret written successfully.")
+	// fmt.Println("Secret written successfully.")
 
-	// Read a secret from the default mount path for KV v2 in dev mode, "secret"
-	secret, err := vaultClient.KVv2("secret").Get(context.Background(), "bingkun-secret-password")
-	if err != nil {
-		log.Fatalf("unable to read secret: %v", err)
-	}
+	// // Read a secret from the default mount path for KV v2 in dev mode, "secret"
+	// secret, err := vaultClient.KVv2("secret").Get(context.Background(), "bingkun-secret-password")
+	// if err != nil {
+	// 	log.Fatalf("unable to read secret: %v", err)
+	// }
 
-	value, ok := secret.Data["bingkun_password"].(string)
-	if !ok {
-		log.Fatalf("value type assertion failed: %T %#v", secret.Data["bingkun_password"], secret.Data["bingkun_password"])
-	}
+	// value, ok := secret.Data["bingkun_password"].(string)
+	// if !ok {
+	// 	log.Fatalf("value type assertion failed: %T %#v", secret.Data["bingkun_password"], secret.Data["bingkun_password"])
+	// }
 
-	if value != "Hashi123" {
-		log.Fatalf("unexpected password value %q retrieved from vault", value)
-	}
+	// if value != "Hashi123" {
+	// 	log.Fatalf("unexpected password value %q retrieved from vault", value)
+	// }
 
-	fmt.Println("Access granted!")	
+	// fmt.Println("Access granted!")	
 }
 
 // func getSecretWithAWSAuthIAM() (string, error) {
