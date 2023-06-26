8b787d83323ae4da7b29bd6bb4736c7f8ab4c291
Bingkun Yang <bingkun.yang@K2QQR0P936.vungle.com>
2023-06-26 20:36:56 +0800

    change path

--- main.go
+++ main.go
@@ -107,7 +107,7 @@ func main() {
 	}
 
 	// get secret from the default mount path for KV v2 in dev mode, "secret"
-	secret, err := vaultClient.KVv2("secret").Get(context.Background(), "creds")
+	secret, err := vaultClient.KVv2("secret").Get(context.Background(), "ops")
 	if err != nil {
 		log.Fatalf("unable to read secret: %s", err)
 	}
